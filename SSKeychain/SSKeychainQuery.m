42993eb4368ae59694392a0bcddb0b73b0b51651
Meiwin <meiwin@pie.co>
2015-02-17 17:18:11 +0800

    setting access group in simulator

--- SSKeychain/SSKeychainQuery.m
+++ SSKeychain/SSKeychainQuery.m
@@ -189,7 +189,7 @@
 		[dictionary setObject:self.account forKey:(__bridge id)kSecAttrAccount];
 	}
 
-#if __IPHONE_3_0 && TARGET_OS_IPHONE && !TARGET_IPHONE_SIMULATOR
+#if __IPHONE_3_0 && TARGET_OS_IPHONE
 	if (self.accessGroup) {
 		[dictionary setObject:self.accessGroup forKey:(__bridge id)kSecAttrAccessGroup];
 	}
